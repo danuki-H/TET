847215db4748ce023fbd392bf967749f7abfc273
danuki.nqh <nguyenquanghuylt2002@gmail.com>
2022-12-10 21:07:47 +0700

    fff

--- scripts.js
+++ scripts.js
@@ -4,8 +4,7 @@ let may2 = document.getElementById('may-2')
 let ca = document.getElementById('ca')
 let nui = document.getElementById('nui')
 let bgThiep = document.getElementById('bg-thiep')
-let text1 = document.getElementById('text1')
-let text2 = document.getElementById('text2')
+let title = document.getElementById('title')
 let lan = document.getElementById('lan')
 let ch1 = document.getElementById('canh-hoa-1')
 let ch2 = document.getElementById('canh-hoa-2')
@@ -18,9 +17,17 @@ window.addEventListener('scroll', function(){
     may1.style.transform = 'translateY(' + position*0.8 + 'px) translateX(' +(-position*0.6) + 'px) scale(' + (position+500)/500 + ')';
     may2.style.transform = 'translateY(' + position*0.7 + 'px) translateX(' +(position*0.5) + 'px) scale(' + (position+400)/500 + ')';
     nui.style.transform = 'translateY(' + (-position*0.2) + 'px)';
-    lan.style.transform = 'translateY(' + (-position*0.6) + 'px)';
-
-    if(position >= rect.height){
-        
+    lan.style.transform = 'translateY(' + (-position*0.7) + 'px)';
+    if(position >= rect.height/2){
+        title.style.left = '50%'
     }
+    if(position >= rect.height*1.2){
+        title.style.left = '-100%'
+    }
+    if(position < rect.height/2){
+        title.style.left = '140%'
+    }
+    ch1.style.transform = 'translateY(' + position*0.6 + 'px)'
+    ch2.style.transform = 'translateY(' + -position*0.4 + 'px)'
+
 })
