fb26065a1d9ae85bdddb4ab8963cbe771b03ea86
danuki.nqh <nguyenquanghuylt2002@gmail.com>
2022-12-11 20:26:08 +0700

    Update loader

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-//
+let loader = document.querySelector('.loader');
+window.addEventListener("load", function(){
+    let items = document.querySelectorAll('.item');
+    loader.style.display = 'none';
+    items.forEach(item => {
+        item.classList.add('active');
+    });
+})
