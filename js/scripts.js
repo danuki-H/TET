b139dd2118653ad67d91ed863d227916ad77a1e9
danuki.nqh <nguyenquanghuylt2002@gmail.com>
2022-12-13 10:30:57 +0700

    Upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,17 @@
 window.addEventListener("load", function(){
     let loader = document.querySelector('.loader');
     let items = document.querySelectorAll('.item');
+    let thiepChuc = document.querySelector('.thiep-chuc');
     loader.style.display = 'none';
     items.forEach(item => {
         item.classList.add('active');
     });
+    document.getElementById('read-more').addEventListener('click', function(){
+        thiepChuc.classList.add('active');
+        thiepChuc.classList.remove('exit');
+    })
+    document.getElementById('thoat').addEventListener('click', function(){
+        thiepChuc.classList.add('exit');
+        thiepChuc.classList.remove('active');
+    })
 })
